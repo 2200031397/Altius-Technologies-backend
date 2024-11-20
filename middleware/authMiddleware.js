41b4195d50f66a284d797faca89d5e868d8df5db
2200031397 <139319227+2200031397@users.noreply.github.com>
2024-11-20 16:22:32 +0530

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,18 +1,15 @@
 const jwt = require('jsonwebtoken');
 
-// Hardcoded JWT Secret Key (replace with a secure key for production)
 const secretKey = 'your_jwt_secret_key';
 
 const authenticateToken = (req, res, next) => {
   const authHeader = req.headers['authorization'];
 
-  // Check if the header is present and starts with 'Bearer'
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
     console.log("Authorization header missing or malformed");
     return res.status(403).send({ message: 'Token required or malformed' });
   }
 
-  // Extract the token by removing 'Bearer ' prefix
   const token = authHeader.split(' ')[1];
 
   jwt.verify(token, secretKey, (err, user) => {
@@ -27,7 +24,7 @@ const authenticateToken = (req, res, next) => {
 
 const authorizeRoles = (...roles) => {
   return (req, res, next) => {
-    console.log("Authorizing role for user:", req.user?.role);
+    
     if (!roles.includes(req.user.role)) {
       console.log("Access denied for role:", req.user.role);
       return res.status(403).send({ message: 'Access denied' });
