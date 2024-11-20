091b14090622f7b6503cb3c03c20a9dcc3542b43
2200031397 <139319227+2200031397@users.noreply.github.com>
2024-11-20 16:23:06 +0530

    Update ticket.js

--- models/ticket.js
+++ models/ticket.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const ticketSchema = new mongoose.Schema({
   title: { type: String, required: true },
   status: { type: String, enum: ['Active', 'Pending', 'Closed'], default: 'Active' },
-  customerName: { type: String, required: true }, // Storing the name separately
-  customerRole: { type: String, required: true }, // Storing the role separately
+  customerName: { type: String, required: true }, 
+  customerRole: { type: String, required: true }, 
   lastUpdatedOn: { type: Date, default: Date.now },
   notes: [
     {
