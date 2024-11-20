cd5060100749588757662ee4f08fc427715d6e83
2200031397 <139319227+2200031397@users.noreply.github.com>
2024-11-20 16:23:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,21 +5,15 @@ const Router = require('./routes/router');
 
 const app = express();
 
-// Middleware
 app.use(express.json());
 app.use(cors());
 
-// MongoDB Connection URL (hardcoded for demonstration purposes)
 const dburl = 'mongodb+srv://charanchowdarynunnam:KzULXYPA8UqWl4R4@seequenze.ngp9l.mongodb.net/?retryWrites=true&w=majority&appName=Seequenze';
 
-// Connect to MongoDB
-mongoose.connect(dburl, {
- 
-})
+mongoose.connect(dburl, {})
   .then(() => console.log('Connected to MongoDB successfully'))
   .catch(err => console.error('DB Connection Error:', err));
 
-// Route handling
 app.use('/api', Router);
 
 const PORT = 3000;
