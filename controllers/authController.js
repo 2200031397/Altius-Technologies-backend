bfb2845f1a2effa2e68e34d88fcef3f90a6aa124
2200031397 <139319227+2200031397@users.noreply.github.com>
2024-11-20 16:19:41 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,12 +2,10 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const Customer = require('../models/customer');
 
-// Hardcoded secret key for demonstration purposes; replace or use an env variable in production.
 const secretKey = 'your_jwt_secret_key'; 
 
 const getCustomers = async (req, res) => {
   try {
-    // Fetch customers with the role 'Customer'
     const customers = await Customer.find({ role: 'Customer' });
     res.status(200).json(customers);
   } catch (error) {
@@ -23,7 +21,7 @@ const register = async (req, res) => {
       return res.status(400).json({ message: 'All fields are required' });
     }
 
-    // Hash the user's password
+    
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const newCustomer = new Customer({
@@ -53,13 +51,13 @@ const login = async (req, res) => {
       return res.status(404).json({ message: 'User not found' });
     }
 
-    // Check the password
+    
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
       return res.status(401).json({ message: 'Invalid password' });
     }
 
-    // Generate JWT
+    
     const token = jwt.sign({ id: user._id, role: user.role,name: user.name}, secretKey, { expiresIn: '1h' });
     res.status(200).json({ message: 'Login successful', token, user });
   } catch (error) {
